be21baa9bfce00fb1e4f79aba3de6a69d2ec2049
manmohanjina <manish63singh@gmail.com>
2024-11-10 21:10:35 +0530

    link react router dom fix

--- src/component/chatApp/login.jsx
+++ src/component/chatApp/login.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
-import { useNavigate} from "react-router-dom"
+import { Link, useNavigate} from "react-router-dom"
 import { ThemeContext } from '../context';
 import Loading from '../loading';
 
